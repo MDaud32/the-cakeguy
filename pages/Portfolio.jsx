c64a93b881ac38d773daa090e2bd00c687a9d39e
MDaud32 <theprofessor543@gmail.com>
2023-03-13 22:27:55 +0500

    fixing portfolio

--- pages/Portfolio.jsx
+++ pages/Portfolio.jsx
@@ -1,316 +1,138 @@
 import Image from "next/image";
-
 import ScrollButton from "@/components/ScrollTop";
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Portfolio = () => {
-  let [isOpen, setIsOpen] = useState(false);
-  let [image, setImage] = useState(0);
-  const [prevDisabled, setprevDisabled] = useState(false);
-  const [nextDisabled, setNextDisabled] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
 
-  function Img1() {
-    setImage(1);
-    setIsOpen(true);
-  }
-  function Img2() {
-    setImage(2);
-    setIsOpen(true);
-  }
-  function Img3() {
-    setImage(3);
-    setIsOpen(true);
-  }
-  function Img4() {
-    setImage(4);
-    setIsOpen(true);
-  }
-  function Img5() {
-    setImage(5);
-    setIsOpen(true);
-  }
-  function Img6() {
-    setImage(6);
-    setIsOpen(true);
-  }
-  function Img7() {
-    setImage(7);
-    setIsOpen(true);
-  }
-  function Img8() {
-    setImage(8);
-    setIsOpen(true);
-  }
-  function Img9() {
-    setImage(9);
-    setIsOpen(true);
-  }
-  function Img10() {
-    setImage(10);
-    setIsOpen(true);
-  }
-  function Img11() {
-    setImage(11);
-    setIsOpen(true);
-  }
-  function Img12() {
-    setImage(12);
-    setIsOpen(true);
-  }
-  function Img13() {
-    setImage(13);
-    setIsOpen(true);
-  }
-  function Img14() {
-    setImage(14);
-    setIsOpen(true);
-  }
-  function Img15() {
-    setImage(15);
-    setIsOpen(true);
-  }
-  function Img16() {
-    setImage(16);
-    setIsOpen(true);
-  }
-  function Img17() {
-    setImage(17);
-    setIsOpen(true);
-  }
-  function Img18() {
-    setImage(18);
-    setIsOpen(true);
-  }
+  // Close Modal
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
-  function switchNext() {
-    if (image === 18) {
-      return setNextDisabled(true);
-    }
-    setprevDisabled(false);
-    setImage(image + 1);
-  }
-  function switchPrev() {
-    if (image === 1) {
-      return setprevDisabled(true);
-    }
-    setNextDisabled(false);
-    setImage(image - 1);
-  }
+  // Previous Image
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(data.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+
+  // Next Image
+  const nextSlide = () => {
+    slideNumber + 1 === data.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
 
   return (
-    <div className="md:w-[77%] lg:w-[84%] ml-auto sm:px-8 lg:px-16 pt-10">
-      <div className="lg:columns-4 md:columns-3 sm:columns-2 columns-1 gap-3 mx-auto space-y-3 px-3">
-        <Image
-          src={"/images/image1.jpg"}
-          onClick={Img1}
-          width={300}
-          height={200}
-          alt="img gallery"
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
+    <div>
+      {openModal && (
+        <div className="sliderWrap">
+          <AiOutlineClose className="btnClose" onClick={handleCloseModal} />
+          <MdNavigateBefore className="btnPrev" onClick={prevSlide} />
+          <MdNavigateNext className="btnNext" onClick={nextSlide} />
+          <div className="fullScreenImage" onClick={() => handleCloseModal()}>
+            <Image
+              src={data[slideNumber].img}
+              alt=""
+              width={400}
+              height={200}
+              className={
+                "object-cover w-[700px] h-[300px] md:h-[500px]  md:mt-0"
+              }
+            />
+          </div>
+        </div>
+      )}
 
-        <Image
-          src={"/images/image2.jpg"}
-          onClick={Img2}
-          width={300}
-          height={200}
-          alt="img gallery"
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          src={"/images/image3.jpg"}
-          onClick={Img3}
-          width={300}
-          height={200}
-          alt="img gallery"
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img4}
-          src={"/images/image4.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img5}
-          src={"/images/image5.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img6}
-          src={"/images/image6.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img7}
-          src={"/images/image7.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img8}
-          src={"/images/image8.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img9}
-          src={"/images/image9.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img10}
-          src={"/images/image10.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img11}
-          src={"/images/image11.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img12}
-          src={"/images/image12.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img13}
-          src={"/images/image13.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img14}
-          src={"/images/image14.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img15}
-          src={"/images/image15.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img16}
-          src={"/images/image16.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img17}
-          src={"/images/image17.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-        <Image
-          onClick={Img18}
-          src={"/images/image18.jpg"}
-          alt="img gallery"
-          width={300}
-          height={200}
-          className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
-        />
-      </div>
-      <>
-        <Transition appear show={isOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-10" onClose={closeModal}>
-            <div className="fixed inset-0 overflow-y-auto">
-              <div className="flex min-h-full items-center justify-center  text-center">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0 scale-95"
-                  enterTo="opacity-100 scale-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100 scale-100"
-                  leaveTo="opacity-0 scale-95">
-                  <Dialog.Panel className="w-[80%] md:w-[60%] h-[60%] md:h-[80%] transform overflow-hidden bg-black/50 text-left align-middle shadow-xl transition-all pb-10">
-                    <button
-                      onClick={() => setIsOpen(false)}
-                      className="text-white text-4xl flex ml-auto px-10 pt-4">
-                      <AiOutlineClose />
-                    </button>
-                    <div className={"flex justify-between"}>
-                      <button
-                        disabled={prevDisabled}
-                        onClick={switchPrev}
-                        className="text-white disabled:text-red-400">
-                        <MdNavigateBefore size={"40"} />
-                      </button>
-
-                      <div className="flex flex-row justify-center">
-                        <Image
-                          src={`/images/image${image}.jpg`}
-                          width={400}
-                          height={200}
-                          className={
-                            "object-cover w-[700px] h-[300px] md:h-[500px]  md:mt-0"
-                          }
-                          alt="green iguana"
-                        />
-                      </div>
-
-                      <button
-                        disabled={nextDisabled}
-                        className="text-white disabled:text-red-400"
-                        onClick={switchNext}>
-                        <MdNavigateNext size={"40"} />
-                      </button>
-                    </div>
-                  </Dialog.Panel>
-                </Transition.Child>
+      <div className="md:w-[77%] lg:w-[84%] ml-auto sm:px-8 lg:px-16 pt-10">
+        <div className="lg:columns-4 md:columns-3 sm:columns-2 columns-1 gap-3 mx-auto space-y-3 px-3">
+          {data.map((slide, index) => {
+            return (
+              <div
+                className="single"
+                key={index}
+                onClick={() => handleOpenModal(index)}>
+                <Image
+                  src={slide.img}
+                  alt=""
+                  width={300}
+                  height={200}
+                  className=" rounded-md object-cover w-3/4 mx-auto sm:w-[300px] xl:w-[400px] transition-transform duration-500 transform-gpu hover:scale-105"
+                />
               </div>
-            </div>
-          </Dialog>
-        </Transition>
-      </>
-      <div className="md:-ml-56">
-        <ScrollButton />
-      </div>{" "}
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Portfolio;
+
+const data = [
+  {
+    img: "/images/image1.jpg",
+  },
+  {
+    img: "/images/image2.jpg",
+  },
+  {
+    img: "/images/image3.jpg",
+  },
+  {
+    img: "/images/image4.jpg",
+  },
+  {
+    img: "/images/image5.jpg",
+  },
+  {
+    img: "/images/image6.jpg",
+  },
+  {
+    img: "/images/image7.jpg",
+  },
+  {
+    img: "/images/image8.jpg",
+  },
+  {
+    img: "/images/image9.jpg",
+  },
+  {
+    img: "/images/image10.jpg",
+  },
+  {
+    img: "/images/image11.jpg",
+  },
+  {
+    img: "/images/image12.jpg",
+  },
+  {
+    img: "/images/image13.jpg",
+  },
+  {
+    img: "/images/image14.jpg",
+  },
+  {
+    img: "/images/image15.jpg",
+  },
+  {
+    img: "/images/image16.jpg",
+  },
+  {
+    img: "/images/image17.jpg",
+  },
+  {
+    img: "/images/image18.jpg",
+  },
+];
